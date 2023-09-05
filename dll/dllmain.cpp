7a723149ecfc937546d4f1771557a4b3d5c04690
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2023-09-05 23:25:47 +0200

    dll cleanup and musics example

--- dll/dllmain.cpp
+++ dll/dllmain.cpp
@@ -1,22 +1,81 @@
-#include "pch.h"
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <cstdlib>
+#include <vector>
+#include <string>
 #include "detours.h"
 
 struct Mix_Music;
+
+typedef Mix_Music* (__cdecl* Mix_LoadMUSFunc)(const char* file);
+typedef int(__cdecl* Mix_PlayMusicFunc)(Mix_Music* music, int loops);
+typedef void(__cdecl* Mix_FreeMusicFunc)(Mix_Music* music);
+
+static Mix_LoadMUSFunc loadmusic = NULL;
+static Mix_PlayMusicFunc playmusic = NULL;
+static std::vector<Mix_Music*> musics;
+
+static void find_musics() {
+    WIN32_FIND_DATAA f;
+    auto hf = FindFirstFileA("allmusics\\*.ogg", &f);
+    if (hf == INVALID_HANDLE_VALUE)
+        return;
+    do {
+        //MessageBoxA(NULL, f.cFileName, "", MB_OK);
+        auto path = std::string("allmusics\\") + f.cFileName;
+        auto mus = loadmusic(path.c_str());
+        if (mus)
+            musics.push_back(mus);
+    } while (FindNextFileA(hf, &f));
+    FindClose(hf);
+}
+
+static void free_musics() {
+    auto freemusic = static_cast<Mix_FreeMusicFunc>(DetourFindFunction("SDL_mixer.dll", "Mix_FreeMusic"));
+    if (!freemusic)
+        return;
+    for (auto mus : musics)
+        freemusic(mus);
+}
+
 int my_Mix_PlayMusic(Mix_Music* music, int loops) {
-    return 0;
+    if (musics.size()) {
+        return playmusic(musics[rand() % musics.size()], loops);
+    }
+    else {
+        return playmusic(music, loops);
+    }
+}
+
+Mix_Music* my_Mix_LoadMUS(const char* file) {
+    static bool inited = false;
+    if (!inited) {
+        find_musics();
+        inited = true;
+    }
+    return loadmusic(file);
 }
 
 static void install(bool value) {
-    auto playmusic = DetourFindFunction("SDL_mixer.dll", "Mix_PlayMusic");
+    loadmusic = static_cast<Mix_LoadMUSFunc>(DetourFindFunction("SDL_mixer.dll", "Mix_LoadMUS"));
+    if (!loadmusic)
+        return;
+    playmusic = static_cast<Mix_PlayMusicFunc>(DetourFindFunction("SDL_mixer.dll", "Mix_PlayMusic"));
     if (!playmusic)
         return;
     DetourTransactionBegin();
     DetourUpdateThread(GetCurrentThread());
-    if (value)
+    if (value) {
+        DetourAttach(&loadmusic, my_Mix_LoadMUS);
         DetourAttach(&playmusic, my_Mix_PlayMusic);
-    else
+    }
+    else {
         DetourDetach(&playmusic, my_Mix_PlayMusic);
+        DetourDetach(&loadmusic, my_Mix_LoadMUS);
+    }
     DetourTransactionCommit();
+    if (!value)
+        free_musics();
 }
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {

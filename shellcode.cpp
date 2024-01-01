9d5d38ad877a0bb276eb63db92e60578f16ab8e8
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2024-01-01 23:57:51 +0100

    cleanup shellcode

--- shellcode.cpp
+++ shellcode.cpp
@@ -53,39 +53,39 @@ __declspec(safebuffers, code_seg("shcode"))VOID NTAPI shellcode(
     auto ppeb = reinterpret_cast<PPEB>(__readfsdword(offsetof(TEB, ProcessEnvironmentBlock)));
     auto link = ppeb->Ldr->InMemoryOrderModuleList.Flink;
 
-    IMAGE_DOS_HEADER* k32Base = NULL;
+    uint8_t* k32Base = NULL;
     do {
         auto entry = reinterpret_cast<LDR_DATA_TABLE_ENTRY*>(CONTAINING_RECORD(link, LDR_DATA_TABLE_ENTRY, InMemoryOrderLinks));
         if (sc_strcaseeq(s_kernel32_dll, (&entry->FullDllName)[1].Buffer)) {
-            k32Base = static_cast<IMAGE_DOS_HEADER*>(entry->DllBase);
+            k32Base = static_cast<uint8_t*>(entry->DllBase);
             break;
         }
         link = link->Flink;
         if (!link || link == ppeb->Ldr->InMemoryOrderModuleList.Flink)
             break;
     } while (1);
-    if (!k32Base || k32Base->e_magic != IMAGE_DOS_SIGNATURE)
+    if (!k32Base || reinterpret_cast<IMAGE_DOS_HEADER*>(k32Base)->e_magic != IMAGE_DOS_SIGNATURE)
         return;
 
     LoadLibraryFunc f_loadLibrary = NULL;
     GetProcAddressFunc f_getprocaddress = NULL;
-    auto k32NtHdr = reinterpret_cast<IMAGE_NT_HEADERS*>(reinterpret_cast<uint8_t*>(k32Base) + k32Base->e_lfanew);
+    auto k32NtHdr = reinterpret_cast<IMAGE_NT_HEADERS*>(k32Base + reinterpret_cast<IMAGE_DOS_HEADER*>(k32Base)->e_lfanew);
     auto k32DirExport = &(k32NtHdr->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXPORT]);
-    auto k32Exports = reinterpret_cast<IMAGE_EXPORT_DIRECTORY*>(reinterpret_cast<uint8_t*>(k32Base) + k32DirExport->VirtualAddress);
-    auto k32NameTable = reinterpret_cast<DWORD*>(reinterpret_cast<uint8_t*>(k32Base) + k32Exports->AddressOfNames);
-    auto k32NameOrdTable = reinterpret_cast<WORD*>(reinterpret_cast<uint8_t*>(k32Base) + k32Exports->AddressOfNameOrdinals);
-    auto k32FuncTable = reinterpret_cast<DWORD*>(reinterpret_cast<uint8_t*>(k32Base) + k32Exports->AddressOfFunctions);
+    auto k32Exports = reinterpret_cast<IMAGE_EXPORT_DIRECTORY*>(k32Base + k32DirExport->VirtualAddress);
+    auto k32NameTable = reinterpret_cast<DWORD*>(k32Base + k32Exports->AddressOfNames);
+    auto k32NameOrdTable = reinterpret_cast<WORD*>(k32Base + k32Exports->AddressOfNameOrdinals);
+    auto k32FuncTable = reinterpret_cast<DWORD*>(k32Base + k32Exports->AddressOfFunctions);
     for (DWORD i = 0; i < k32Exports->NumberOfNames; i++) {
         auto name = reinterpret_cast<char*>(k32Base) + k32NameTable[i];
         if (!f_loadLibrary && sc_streq(s_loadlibrary, name)) {
             auto ord = k32NameOrdTable[i];
             auto func = k32FuncTable[ord];
-            f_loadLibrary = reinterpret_cast<LoadLibraryFunc>(reinterpret_cast<uint8_t*>(k32Base) + func);
+            f_loadLibrary = reinterpret_cast<LoadLibraryFunc>(k32Base + func);
         }
         else if (!f_getprocaddress && sc_streq(s_getprocaddress, name)) {
             auto ord = k32NameOrdTable[i];
             auto func = k32FuncTable[ord];
-            f_getprocaddress = reinterpret_cast<GetProcAddressFunc>(reinterpret_cast<uint8_t*>(k32Base) + func);
+            f_getprocaddress = reinterpret_cast<GetProcAddressFunc>(k32Base + func);
         }
     }
 
